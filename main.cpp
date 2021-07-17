399e2480df6c1144c2a68856434476e434803db1
Jakob Šrumpf <48151998+Kikanon@users.noreply.github.com>
2021-07-17 16:44:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-
-
-    w.show();//zagon
+    QApplication a(argc, argv);//zagon aplikacije
+    
+    MainWindow w;//aplikaciji dodamo main window
+    w.show();
+    
     return a.exec();
 }
